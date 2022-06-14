b2e8cd72c864a682c62c3b7ecb00fa4782482851
PointSource <47544779+point-source@users.noreply.github.com>
2022-06-14 15:03:27 -0400

    Return null from getInputThumbnail if a thumbnail could not be fetched

--- lib/src/models/params/get_input_thumbnail.dart
+++ lib/src/models/params/get_input_thumbnail.dart
@@ -6,9 +6,10 @@ import 'package:barco_event_master/barco_event_master.dart';
 extension GetInputThumbnail on EventMaster {
   /// Gets a png thumbnail of the content on the specified input and
   /// returns it as a list of 8-bit unsigned integers
-  Future<Uint8List> getInputThumbnail(int inputId) async {
+  Future<Uint8List?> getInputThumbnail(int inputId) async {
     final uri = Uri.parse('http://$ipAddress/uploads/input_$inputId.png');
     final r = await http.get(uri);
+    if (r.statusCode != 200) return null;
 
     return r.bodyBytes;
   }
