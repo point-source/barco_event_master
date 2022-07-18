5655777242d7b877a79066b135ec402b94773d69
PointSource <47544779+point-source@users.noreply.github.com>
2022-07-18 16:23:49 -0400

    Improve docs

--- lib/src/barco_event_master_base.dart
+++ lib/src/barco_event_master_base.dart
@@ -4,17 +4,27 @@ import 'dart:math';
 import 'package:barco_event_master/barco_event_master.dart';
 import 'package:http/http.dart' as http;
 
+/// Main Event Master control class
+///
+/// Requires an IP Address to connect to an Event Master system
 class EventMaster {
+  /// Initializes the EventMaster class with an IP address
+  ///
+  /// Note that this does not test for connectivity with the event master.
+  /// It only configures the IP address to be used for requests.
   EventMaster({
     required this.ipAddress,
   });
 
+  /// IP Address to be used to communicate with the Event Master system
   final String ipAddress;
 
   Uri get _uri => Uri.parse('http://$ipAddress:9999');
 
   String get _randomId => Random().nextInt(99999).toString();
 
+  /// Sends a JSON RPC compliant request to the Event Master system and
+  /// returns an [EventMasterResponse]
   Future<EventMasterResponse<dynamic>> send({
     required String method,
     required Map<String, dynamic> params,
@@ -34,6 +44,7 @@ class EventMaster {
   }
 }
 
+/// Adds the [addIfPresent] method to Map
 extension AddIfPresent<K, V> on Map<K, V> {
   /// Adds a key/value pair as long as the value is not null or an empty String
   void addIfPresent(K key, V value) {
--- lib/src/models/event_master_response.dart
+++ lib/src/models/event_master_response.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+/// Generic response from an Event Master system
 class EventMasterResponse<T> {
   /// The data returned by the invoked method. If an error occurred while
   /// invoking the method, this value must be null.
@@ -12,12 +13,15 @@ class EventMasterResponse<T> {
   /// The receiver of the request must reply with a valid response to all received requests.
   final String id;
 
+  /// Creates an [EventMasterResponse]
   EventMasterResponse({
     this.result,
     this.error,
     required this.id,
   });
 
+  /// Copies an [EventMasterResponse] and optionally allows mutation
+  /// of the parameters and/or the generic type
   EventMasterResponse<T2> copyWith<T2>({
     EventMasterResult<T2>? result,
     String? error,
@@ -30,6 +34,8 @@ class EventMasterResponse<T> {
     );
   }
 
+  /// Creates an [EventMasterResponse] from a Map object containing values
+  /// which correspond to the parameters of [EventMasterResponse]
   factory EventMasterResponse.fromMap(Map<String, dynamic> map) {
     return EventMasterResponse(
       result: EventMasterResult.fromMap(map['result']),
@@ -38,6 +44,8 @@ class EventMasterResponse<T> {
     );
   }
 
+  /// Creates an [EventMasterResponse] from a JSON string containing values
+  /// which correspond to the parameters of [EventMasterResponse]
   factory EventMasterResponse.fromJson(String source) =>
       EventMasterResponse.fromMap(json.decode(source));
 
@@ -46,7 +54,10 @@ class EventMasterResponse<T> {
       'EventMasterResponse(result: $result, error: $error, id: $id)';
 }
 
+/// Contains data returned from the event master system encapsulated
+/// inside an [EventMasterResponse]
 class EventMasterResult<T> {
+  /// Initializes an [EventMasterResult]
   EventMasterResult({
     required this.success,
     this.response,
@@ -54,14 +65,20 @@ class EventMasterResult<T> {
 
   /// 0=success, anything else is an error
   final int success;
+
+  /// Data returned from the Event Master in response to an [EventMasterRequest]
   final T? response;
 
+  /// Copies an [EventMasterResult] and optionally allows mutation
+  /// of the parameters and/or the generic type
   EventMasterResult<T2> copyWith<T2>({int? success, T2? response}) =>
       EventMasterResult<T2>(
         success: success ?? this.success,
         response: T is T2 ? response ?? this.response as T2 : response,
       );
 
+  /// Creates an [EventMasterResult] from a Map object containing values
+  /// which correspond to the parameters of [EventMasterResult]
   factory EventMasterResult.fromMap(Map<String, dynamic> map) {
     return EventMasterResult(
       success: map['success'],
